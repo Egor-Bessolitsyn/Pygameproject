cf065831a13095f0396a0bc51f4eccb317dd49d4
besso <bessolitsynegor@yandex.ru>
2023-01-13 23:02:40 +0300

    Добавление второго игрока управление первого изменено на WSAD кнопка для одиночной игры заблокирована

--- 1.py
+++ 1.py
@@ -1,7 +1,9 @@
 import pygame
 import os
 import sys
+import random
 #or (300 < pygame.mouse.get_pos()[0] < 500 and 460 < pygame.mouse.get_pos()[1] < 500)
+#(300 < pygame.mouse.get_pos()[0] < 500 and 390 < pygame.mouse.get_pos()[1] < 450)
 FPS = 50
 size = WIDTH, HEIGHT = 800, 600
 clock = pygame.time.Clock()
@@ -50,7 +52,7 @@ class Button:
                  action()
 
 
-class AnimatedSprite(pygame.sprite.Sprite):
+class Enemy(pygame.sprite.Sprite):
     def __init__(self, sheet, columns, rows, x, y):
         super().__init__(all_sprites)
         self.frames = []
@@ -59,8 +61,8 @@ class AnimatedSprite(pygame.sprite.Sprite):
         self.image = self.frames[self.cur_frame]
         self.rect = self.rect.move(x, y)
         self.rect = self.image.get_rect()
-        self.rect.x = 100
-        self.rect.y = 100
+        self.rect.x = 200
+        self.rect.y = 200
         self.tick_time = 0
 
     def cut_sheet(self, sheet, columns, rows):
@@ -97,25 +99,72 @@ class AnimatedSprite(pygame.sprite.Sprite):
                 self.rect.x -= 1
 
 
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = 100
+        self.tick_time = 0
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self, *args):
+        if args and args[0].type == pygame.KEYDOWN:
+            self.tick_time += 1
+            # if self.tick_time % 10 == 1:
+            #     self.tick_time %= 10
+            #     self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            #     self.image = self.frames[self.cur_frame]
+            if pygame.key.get_pressed()[pygame.K_w]:
+                self.cur_frame = 0
+                self.image = self.frames[self.cur_frame]
+                self.rect.y -= 1
+            elif pygame.key.get_pressed()[pygame.K_s]:
+                self.cur_frame = 1
+                self.image = self.frames[self.cur_frame]
+                self.rect.y += 1
+            elif pygame.key.get_pressed()[pygame.K_d]:
+                self.cur_frame = 2
+                self.image = self.frames[self.cur_frame]
+                self.rect.x += 1
+            elif pygame.key.get_pressed()[pygame.K_a]:
+                self.cur_frame = 3
+                self.image = self.frames[self.cur_frame]
+                self.rect.x -= 1
+
+
 def terminate():
     pygame.quit()
     sys.exit()
 
 
 def start_screen():
-    button1 = Button(200, 40, (255, 0, 0))
-    #button2 = Button(200, 40, (255, 0, 0))
+    button_sologame = Button(200, 40, (255, 0, 0))
+    button_pvpgame = Button(200, 40, (255, 0, 0))
     fon = pygame.transform.scale(load_image('fon.png'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
-    button1.draw(300, 390, '1 Player')
-    #button2.draw(300, 460, '2 Players')
+    button_sologame.draw(300, 390, '1 Player')
+    button_pvpgame.draw(300, 460, '2 Players')
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             elif (event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN) \
-                    and ((300 < pygame.mouse.get_pos()[0] < 500 and 390 < pygame.mouse.get_pos()[1] < 450)):
+                    and ((300 < pygame.mouse.get_pos()[0] < 500 and 460 < pygame.mouse.get_pos()[1] < 500)):
                 return  # начинаем игру
         pygame.display.flip()
         clock.tick(FPS)
@@ -126,6 +175,7 @@ if __name__ == '__main__':
 
     all_sprites = pygame.sprite.Group()
     tank = AnimatedSprite(load_image("yellow_tanks.png"), 4, 1, 50, 50)
+    enemy = Enemy(load_image("yellow_tanks.png"), 4, 1, 50, 50)
 
     clock = pygame.time.Clock()
 
