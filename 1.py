327d8a54fcc30eca3a72683ca87c87b82ad51f42
Oleg <oleg.zayac10@gmail.com>
2023-01-15 17:15:17 +0300

    исправление(теперь пули видно)

--- 1.py
+++ 1.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import sys
 
+# №фывыфв
 # or (300 < pygame.mouse.get_pos()[0] < 500 and 460 < pygame.mouse.get_pos()[1] < 500)
 # (300 < pygame.mouse.get_pos()[0] < 500 and 390 < pygame.mouse.get_pos()[1] < 450)
 FPS = 100
