9aed97ccf5243c500a5af0d68c13d1dc05d4f02b
Oleg <oleg.zayac10@gmail.com>
2023-02-28 14:30:54 +0300

    НЕДОРАБОТАНА. С помощью мыши можно управлять танком, НО программа работает со сбоями

--- 1.py
+++ 1.py
@@ -206,32 +206,42 @@ def Finding_Path(start, end):
     start = start
     m[start[0]][start[1]] = 1
     end = end
+    visited_old = [(start)]
+    visited_new = []
 
     def make_step(k):
-        for i in range(len(m)):
-            for j in range(len(m[i])):
-                if m[i][j] == k:
-                    if i > 0 and m[i - 1][j] == 0 and a[i - 1][j] == 0:
-                        if j < 23 and m[i - 1][j + 1] == 0 and a[i - 1][j + 1] == 0:
-                            m[i - 1][j] = k + 1
-                            # m[i - 1][j + 1] = k + 1
-                    if j > 0 and m[i][j - 1] == 0 and a[i][j - 1] == 0:
-                        if i < 23 and m[i + 1][j - 1] == 0 and a[i + 1][j - 1] == 0:
-                            m[i][j - 1] = k + 1
-                            # m[i + 1][j - 1] = k + 1
-                    if i < len(m) - 2 and m[i + 1][j] == 0 and a[i + 1][j] == 0 and not a[i + 2][j]:
-                        if j < 22 and m[i + 1][j + 1] == 0 and a[i + 1][j + 1] == 0 and not a[i + 2][j + 1]:
-                            m[i + 1][j] = k + 1
-                            # m[i + 1][j + 1] = k + 1
-                    if j < len(m[i]) - 2 and m[i][j + 1] == 0 and a[i][j + 1] == 0 and not a[i][j + 2]:
-                        if i < 22 and m[i + 1][j + 1] <= k + 1 and a[i + 1][j + 1] == 0 and not a[i + 1][j + 2]:
-                            m[i][j + 1] = k + 1
-                            # m[i + 1][j + 1] = k + 1
+        for cell in visited_old:
+            i, j = cell[0], cell[1]
+            if m[i][j] == k:
+                if i > 0 and j < len(m[i]) - 1 and m[i - 1][j] == 0 and a[i - 1][j] == 0 and not a[i - 1][j + 1]:
+                    # if j < 23 and m[i - 1][j + 1] == 0 and a[i - 1][j + 1] == 0:
+                    m[i - 1][j] = k + 1
+                    visited_new.append((i - 1, j))
+                    # m[i - 1][j + 1] = k + 1
+                if j > 0 and i < 23 and m[i][j - 1] == 0 and a[i][j - 1] == 0 and not a[i + 1][j - 1]:
+                    # if i < 23 and m[i + 1][j - 1] == 0 and a[i + 1][j - 1] == 0:
+                    m[i][j - 1] = k + 1
+                    visited_new.append((i, j - 1))
+                    # m[i + 1][j - 1] = k + 1
+                if i < len(m) - 2 and j < 23 and m[i + 1][j] == 0 and a[i + 1][j] == 0 and not a[i + 2][j] and not \
+                a[i + 1][j + 1] and not a[i + 2][j + 1]:
+                    # if j < 22 and m[i + 1][j + 1] == 0 and a[i + 1][j + 1] == 0 and not a[i + 2][j + 1]:
+                    m[i + 1][j] = k + 1
+                    visited_new.append((i + 1, j))
+                    # m[i + 1][j + 1] = k + 1
+                if j < len(m[i]) - 2 and i < 23 and m[i][j + 1] == 0 and a[i][j + 1] == 0 and not a[i][j + 2] and not \
+                a[i + 1][j + 1] and not a[i + 1][j + 2]:
+                    # if i < 22 and m[i + 1][j + 1] <= k + 1 and a[i + 1][j + 1] == 0 and not a[i + 1][j + 2]:
+                    m[i][j + 1] = k + 1
+                    visited_new.append((i, j + 1))
+                    # m[i + 1][j + 1] = k + 1
 
     k = 0
     while m[end[0]][end[1]] == 0:
         k += 1
         make_step(k)
+        visited_old = visited_new[:]
+        visited_new = []
 
     i, j = end
     k = m[i][j]
